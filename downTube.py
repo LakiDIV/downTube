d7f016cf1b433003a3fd2485f4307d4725964c09
Lakshika Madhuranga <lakshikamadhuranga21@gmail.com>
2022-11-21 13:38:19 +0530

    Fixed and updated
    Added functionality to download playlists

--- downTube.py
+++ downTube.py
@@ -1,93 +1,94 @@
 from pytube import YouTube
 from pytube import Playlist
 from pathlib import Path
-import csv
 import sys
+
 
 # root
 SAVE_PATH = str(Path.home() / "Downloads/downTube")
-URLS_FILE = 'url.csv'
-count = 0
+URLS_FILE = 'url.txt'
+videos_count = 0
+playlist_count = 0
+
 
 def main():
+    
     link = input("Enter the YouTube video URL: ")
 
-    if link:
-        Download(link)
-        sys.exit()
-
-    links = Csv_to_list(URLS_FILE)
+    links = txt_list(URLS_FILE)
     links_count = len(links)
-    print(links_count, 'added to queue')
+
+    print(links_count, 'links added to queue')
+    print()
 
     if links:
         for link in links:
-            Download(str(link))
+            Download(link)
+
     Status(links_count)
 
 
 # Download and Save
 def Download(link):
-    global count
+    global videos_count
+    global playlist_count
     is_playlist = False
     
     # Checking for playlist
     # ! BUG - Not working
     if 'playlist' in link:
         playlist = Playlist(link)
-        print(f'Playlist: ')
+        print()
+        print(f'Searching playlist: {playlist.title}')
         is_playlist = True
     else:
         video = YouTube(link)
     
     # Downloading playlist
     if is_playlist:
-        print(len(playlist.video_urls), 'videos in the playlist')
-        for url in playlist.video_urls:
-            print(url)
-        for video in playlist.videos:
-            print(f'Downloading: ')
-            try:
-                video.streams.get_highest_resolution().download(SAVE_PATH)
-            except:
-                print("An error has occurred")
-        count += 1
+        print('Found', len(playlist.video_urls), 'videos')
+        playlist_count += 1
 
-        # ! Recursion - Extract url one by one
+        for video in playlist.video_urls:
+            Download(video)
+        print()
 
     # Downloading single video
     else:
         print(f'Downloading: {video.title}')
-        video = video.streams.get_highest_resolution()
         try:
-            video.download(SAVE_PATH)
-            count += 1
-            print("Done")
+            video.streams.get_highest_resolution().download(SAVE_PATH)
+            videos_count += 1
+            print("- Done")
         except:
             print("An error has occurred")
         
             
-
-# Converting CSV to a list
-def Csv_to_list(file):
-    print('Checking url.csv')
+# handling the text file
+def txt_list(file):
+    print('Checking url.txt')
     try:
         with open(file, 'r') as urls:
-            reader = csv.reader(urls)
-            return list(reader)
+            return urls.readlines()
     except FileNotFoundError:
         sys.exit(f"url.csv - not found")
 
+
 # Printing report
 def Status(links):
-    if links == count:
+    print()
+
+    if links > videos_count:
+        print(links-playlist_count, 'links has rejected.')
+        print(videos_count, 'out of', links, "download has completed successfully.")
+    
+    print(videos_count, 'videos has downloaded.')
+
+    if videos_count >= links:
         print("Download is completed successfully.")
-    elif count > 0:
-        print(count, 'out of', links, "download has completed successfully.")
-    else:
+    elif videos_count == 0:
         print("Nothing has downloaded.")
-
-
+    print()
 
 
 if __name__ == "__main__":
