d690052d5e5ce6fa231193650dabcf0015829655
Lakshika Madhuranga <lakshikamadhuranga21@gmail.com>
2022-12-07 10:41:41 +0530

    Update Tracker.py

--- Tracker.py
+++ Tracker.py
@@ -6,10 +6,12 @@ class Tracker:
     All trackers
     """
 
-    def __init__(self, url_count) -> None:
+    def __init__(self, url_count=0, playlist_count=0) -> None:
         if not url_count:
             raise ModuleNotFoundError(colored(f'Missing URLs\n', 'red'))
         self.url_count = url_count
+        self.playlist_count = playlist_count
+
         # self.url_pass_count = 0
         # self.url_fail_count = 0
         # self.url_skipped_count = 0
